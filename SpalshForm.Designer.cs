615039db908800b08aeb59dc6bd8ac42e64ea640
Maria <132623049+CodeRookie0@users.noreply.github.com>
2023-07-03 21:26:19 +0200

    Update SpalshForm.Designer.cs
    Mistake in a word : "MANGEMENT" -> "MANAGEMENT"

--- SpalshForm.Designer.cs
+++ SpalshForm.Designer.cs
@@ -70,7 +70,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(488, 42);
             this.label1.TabIndex = 1;
-            this.label1.Text = "MINIMARKET MANGEMENT SYSTEM";
+            this.label1.Text = "MINIMARKET MANAGEMENT SYSTEM";
             // 
             // progressBar
             // 
